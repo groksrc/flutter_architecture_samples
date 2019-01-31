0ec4985bfe07f20a5a80734438247440db035db0
Drew Cain <groksrc@gmail.com>
2019-01-31 10:58:57 -0600

    renamed flutter_flutter_redux to flutter_redux

---
+++ example/flutter_redux/test/middleware_test.dart
@@ -0,0 +1,57 @@
+// Copyright 2018 The Flutter Architecture Sample Authors. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found
+// in the LICENSE file.
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:redux/redux.dart';
+import 'package:todos_repository_base/todos_repository_base.dart';
+
+import '../lib/actions/actions.dart';
+import '../lib/middleware/store_todos_middleware.dart';
+import '../lib/models/models.dart';
+import '../lib/reducers/app_state_reducer.dart';
+
+class MockTodosRepository extends Mock implements TodosStorageBase {}
+
+main() {
+  group('Save State Middleware', () {
+    test('should load the todos in response to a LoadTodosAction', () {
+      final repository = MockTodosRepository();
+      final store = Store<AppState>(
+        appReducer,
+        initialState: AppState.loading(),
+        middleware: createStoreTodosMiddleware(repository),
+      );
+      final todos = [
+        TodoEntity("Moin", "1", "Note", false),
+      ];
+
+      when(repository.loadTodos()).thenAnswer((_) => Future.value(todos));
+
+      store.dispatch(LoadTodosAction());
+
+      verify(repository.loadTodos());
+    });
+
+    test('should save the state on every update action', () {
+      final repository = MockTodosRepository();
+      final store = Store<AppState>(
+        appReducer,
+        initialState: AppState.loading(),
+        middleware: createStoreTodosMiddleware(repository),
+      );
+      final todo = Todo("Hallo");
+
+      store.dispatch(AddTodoAction(todo));
+      store.dispatch(ClearCompletedAction());
+      store.dispatch(ToggleAllAction());
+      store.dispatch(TodosLoadedAction([Todo("Hi")]));
+      store.dispatch(ToggleAllAction());
+      store.dispatch(UpdateTodoAction("", Todo("")));
+      store.dispatch(DeleteTodoAction(""));
+
+      verify(repository.saveTodos(any)).called(7);
+    });
+  });
+}
