3f3563fe1b877490137f6d47861766c55d7e94f9
Drew Cain <groksrc@gmail.com>
2019-01-31 08:18:18 -0600

    more renaming

---
+++ example/flutter_flutter_redux/lib/reducers/loading_reducer.dart
@@ -0,0 +1,16 @@
+// Copyright 2018 The Flutter Architecture Sample Authors. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found
+// in the LICENSE file.
+
+import 'package:redux/redux.dart';
+
+import '../actions/actions.dart';
+
+final loadingReducer = combineReducers<bool>([
+  TypedReducer<bool, TodosLoadedAction>(_setLoaded),
+  TypedReducer<bool, TodosNotLoadedAction>(_setLoaded),
+]);
+
+bool _setLoaded(bool state, action) {
+  return false;
+}
